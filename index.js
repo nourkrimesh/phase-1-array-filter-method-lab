01b9aa696fbdf54382435fb806c3e66da6564a63
Nour Krimesh <nour.krimesh@gmail.com>
2023-07-05 23:57:01 +0300

    Complete assignment

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Code your solution here
+
+function findMatching(drivers, driverName) {
+    return drivers.filter(driver => driver.toLowerCase() === driverName.toLowerCase());
+}
+
+function fuzzyMatch(drivers, str) {
+    return drivers.filter(driver => driver.match(`^${str}`));
+}
+
+function matchName(drivers, str) {
+    return drivers.filter((driver) => driver.name === str);
+}
